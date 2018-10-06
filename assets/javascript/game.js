594421a0cd1dfaebf4c0e54bce3f7e4db5a70456
Tara Threewits <TaraThreewits@users.noreply.github.com>
2018-10-06 19:11:09 -0400

    adding css

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+//Array that lists out the options for the options (a - z)
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+//Variables to hold the wins and losses
+var wins = 0
+var losses = 0
+
+//Variables to hold guesses
+var guessesLeft = ""
+var currentGuesses = "" 
+
+var directionsText = document.getElementById("directions-text")
